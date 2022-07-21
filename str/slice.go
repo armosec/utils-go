439b288abd43bff01e8e1d68d3eab29f2ed36c2b
David Wertenteil <dwertent@armosec.io>
2022-07-21 08:32:34 +0300

    adding slice func

--- str/slice.go
+++ str/slice.go
@@ -1,6 +1,9 @@
 package str
 
-import "strings"
+import (
+	"encoding/json"
+	"strings"
+)
 
 // StringInSlice return true if string found in slice of strings
 func StringInSlice(strSlice []string, str string) bool {
@@ -38,4 +41,31 @@ func SliceStringToUnique(strSlice []string) []string {
 		strMap[strSlice[i]] = nil
 	}
 	return MapStringToSlice(strMap)
+}
+
+// RemoveIndexFromStringSlice -
+func RemoveIndexFromStringSlice(s *[]string, index int) {
+	(*s)[index] = (*s)[len(*s)-1]
+	(*s)[len(*s)-1] = ""
+	*s = (*s)[:len(*s)-1]
+}
+
+// MergeSliceAndMap merge a list and keys of map
+func MergeSliceAndMap(s []string, m map[string]string) map[string]string {
+	merged := make(map[string]string)
+	for i := range s {
+		if v, ok := m[s[i]]; ok {
+			merged[s[i]] = v
+		}
+	}
+	return merged
+}
+
+// ObjectToString Convert an object to a json string
+func ObjectToString(obj interface{}) string {
+	bm, err := json.Marshal(obj)
+	if err != nil {
+		return ""
+	}
+	return string(bm)
 }
--- str/slice_test.go
+++ str/slice_test.go
@@ -39,3 +39,85 @@ func TestSliceStringToUnique(t *testing.T) {
 	assert.ElementsMatch(t, SliceStringToUnique([]string{}), []string{})
 	assert.ElementsMatch(t, SliceStringToUnique([]string{"a", "b", "b", "a"}), []string{"a", "b"})
 }
+
+func TestRemoveIndexFromStringList(t *testing.T) {
+	type sliceTestSuite struct {
+		origin   []string
+		expected []string
+		index    int
+	}
+
+	tests := map[string]sliceTestSuite{
+		"remove first": {
+			origin:   []string{"a", "b", "c"},
+			expected: []string{"c", "b"},
+			index:    0,
+		},
+		"remove middle": {
+			origin:   []string{"a", "b", "c"},
+			expected: []string{"a", "c"},
+			index:    1,
+		},
+		"remove end": {
+			origin:   []string{"a", "b", "c"},
+			expected: []string{"a", "b"},
+			index:    2,
+		},
+	}
+
+	for i, v := range tests {
+		RemoveIndexFromStringSlice(&v.origin, v.index)
+		assert.Equal(t, len(v.expected), len(v.origin), i)
+		assert.Equal(t, v.expected[0], v.origin[0], i)
+		assert.Equal(t, v.expected[1], v.origin[1], i)
+	}
+}
+
+func TestMergeSliceAndMap(t *testing.T) {
+	type mapMergeTestCase struct {
+		originSlice []string
+		originMap   map[string]string
+		expectedMap map[string]string
+	}
+
+	tests := map[string]mapMergeTestCase{
+		"remove a single key": {
+			originSlice: []string{"a", "b", "c"},
+			originMap:   map[string]string{"a": "A", "c": "C", "d": "D"},
+			expectedMap: map[string]string{"a": "A", "c": "C"},
+		},
+		"map should not change": {
+			originSlice: []string{"a", "b", "c", "d"},
+			originMap:   map[string]string{"a": "A", "c": "C", "d": "D"},
+			expectedMap: map[string]string{"a": "A", "c": "C", "d": "D"},
+		},
+		"map should be empty": {
+			originSlice: []string{"e", "f"},
+			originMap:   map[string]string{"a": "A", "c": "C", "d": "D"},
+			expectedMap: map[string]string{},
+		},
+		"empty slice": {
+			originSlice: []string{},
+			originMap:   map[string]string{"a": "A", "c": "C", "d": "D"},
+			expectedMap: map[string]string{},
+		},
+		"empty map": {
+			originSlice: []string{"e", "f"},
+			originMap:   map[string]string{},
+			expectedMap: map[string]string{},
+		},
+		"both are empty": {
+			originSlice: []string{},
+			originMap:   map[string]string{},
+			expectedMap: map[string]string{},
+		},
+	}
+
+	for i, v := range tests {
+		r := MergeSliceAndMap(v.originSlice, v.originMap)
+		assert.Equal(t, len(v.expectedMap), len(r), i)
+		for k, v := range v.expectedMap {
+			assert.Equal(t, v, r[k], i)
+		}
+	}
+}
